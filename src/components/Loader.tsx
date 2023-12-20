4d500aca7c1b0e0b439c2cfe8b5b8338dada4072
Jack <lildenzil@gmail.com>
2023-12-20 16:03:41 -0500

    Create Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { FileUpload } from "primereact/fileupload";
+import { CSVToArray } from '../utils/format';
+import { Dialog } from "primereact/dialog";
+import { ProgressSpinner } from "primereact/progressspinner";
+
+type LoaderProps = {
+    loading: boolean,
+    setLoading: (value: boolean) => void,
+};
+
+export const Loader = ({ loading, setLoading }: LoaderProps) => {
+
+    return (
+        <>
+            <Dialog className="h-12rem" header="Generating Amazing Content..." visible={loading} closable={false} onHide={() => setLoading(false)}>
+                <div className="centered-spinner">
+                    <ProgressSpinner />
+                </div>
+            </Dialog>
+        </>
+    )};
