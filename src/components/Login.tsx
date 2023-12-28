0c0f5cc4a1f3bb7022f36ba816382107f024c684
Jack <lildenzil@gmail.com>
2023-12-28 12:59:47 -0500

    Error message for login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dialog } from "primereact/dialog";
 import { InputText } from "primereact/inputtext";
+import { Message } from "primereact/message";
 
 type LoginProps = {
     user: string,
@@ -9,23 +10,30 @@ type LoginProps = {
     setIsLogin: (value: boolean) => void
 };
 
-const checkCredentials = (user: string, setIsLogin: (value: boolean) => void) => {
+const checkCredentials = (user: string, setIsLogin: (value: boolean) => void, setError: (value: boolean) => void) => {
     if (user.toLowerCase() === 'meaghan') {
         setIsLogin(true);
+    } else {
+        setError(true)
     }
 };
 
 export const Login = ({ user, setUser, isLogin, setIsLogin }: LoginProps) => {
-    React.useEffect(() => {
-        checkCredentials(user, setIsLogin);
-    }, [user, setIsLogin]);
+    const [isError, setError] = useState(false);
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            checkCredentials(user, setIsLogin, setError)
+        }
+    }
 
     return (
         <Dialog className="h-10rem w-15rem" visible={!isLogin} onHide={() => alert('Correct user must be entered to proceed!')}>
             <span className="p-float-label mt-4">
-                <InputText id="in" value={user} onChange={(e) => setUser(e.currentTarget.value)} />
+                <InputText id="in" value={user} onChange={(e) => setUser(e.currentTarget.value)} onKeyDown={handleKeyPress} />
                 <label htmlFor="in">Enter User</label>
             </span>
+            {isError && <Message severity="error" text="Invalid Credentials" />}
         </Dialog>
     );
 };
