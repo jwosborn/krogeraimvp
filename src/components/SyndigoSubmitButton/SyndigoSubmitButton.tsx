512dc2600bd30bc979e84fd554a23b90232637e6
Jack <lildenzil@gmail.com>
2024-03-26 18:21:16 -0400

    Add bullet limits

--- src/components/SyndigoSubmitButton/SyndigoSubmitButton.tsx
+++ src/components/SyndigoSubmitButton/SyndigoSubmitButton.tsx
@@ -15,47 +15,32 @@ const parseBullets = (bulletsString: string): { [key: string]: string} => {
     const bulletsArray = bulletsString.trim().split('\n');
     
     const bulletsJSON = bulletsArray.reduce((acc, bullet, index) => {
-        const key = `Feature - Benefit Bullet ${index + 1}`;
-        acc[key] = bullet;
+        if (index < 5) { // There are only bullets 1 - 6 in Syndigo
+            const key = `Feature - Benefit Bullet ${index + 1}`;
+            acc[key] = bullet;
+        }
         return acc;
     }, {} as { [key: string]: string });
     
-    // console.log(bulletsJSON);
     return bulletsJSON;
 }
 
 const handleClick = (products: any[], URL) => {
-    
-    // console.log(products)
-    let ProductMap = products.map(product => {
-        const bullets = parseBullets(product.bullets);
-        return {
-            "Product Name": product?.Product_Title,
-            "GTIN": product?.UPC,
-            "Marketing Copy": product?.description,
-            "Customer Facing Size": product?.Size,
-            ...bullets,
-        }
-    })
-
-    console.log(ProductMap);
 
     let formatProduct = {
-        products:[{
-            "GTIN": "00053883224347",
-            "Marketing Copy": "test" ,
-            "Feature - Benefit Bullet 1": 'Testing bullet 1',
-            "Feature - Benefit Bullet 2": 'Testing bullet 2',
-            "Feature - Benefit Bullet 3": 'Testing bullet 3',
-            "Feature - Benefit Bullet 4": 'Testing bullet 4',
-            "Feature - Benefit Bullet 5": 'Testing bullet 5',
-            "Feature - Benefit Bullet 6": 'Testing bullet 6',
-            
-
-        }]
+        products: products.map(product => {
+            const bullets = parseBullets(product.bullets);
+            return {
+                "Product Name": product?.Product_Title,
+                "GTIN": product?.UPC,
+                "Marketing Copy": product?.description,
+                "Customer Facing Size": product?.Size,
+                ...bullets,
+            }
+        })
     }
 
-    // axios.post(URL + 'update-products', ProductMap)
+    axios.post(URL + 'update-products', formatProduct)
 }
 
 export const SyndigoSubmitButton = ({ products, URL }: SyndigoSubmitButtonProps) => {
