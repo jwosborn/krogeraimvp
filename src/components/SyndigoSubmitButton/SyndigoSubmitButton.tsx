fd6c3a13514aef3ee6e1c3f6a79b5999dacd6d00
Jack <lildenzil@gmail.com>
2024-04-02 11:54:49 -0400

    Response dialog

--- src/components/SyndigoSubmitButton/SyndigoSubmitButton.tsx
+++ src/components/SyndigoSubmitButton/SyndigoSubmitButton.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import { FileUpload } from "primereact/fileupload";
 import { Button } from "primereact/button";
 import axios from "axios";
 import { Dialog } from "primereact/dialog";
-import { InputTextarea } from "primereact/inputtextarea";
 
 type SyndigoSubmitButtonProps = {
     products: any[],
@@ -13,7 +11,9 @@ type SyndigoSubmitButtonProps = {
 
 export const SyndigoSubmitButton = ({ products, URL, buttonText }: SyndigoSubmitButtonProps) => {
     const [showDialog, setShowDialog] = useState(false);
+    const [showResult, setShowResult] = useState(false);
     const [showTitles, setShowTitles] = useState([]);
+    const [responses, setResponses] = useState([]);
 
     const parseBullets = (bulletsString: string): { [key: string]: string} => {
         if (bulletsString === undefined){
@@ -43,6 +43,25 @@ export const SyndigoSubmitButton = ({ products, URL, buttonText }: SyndigoSubmit
           </div>
         );
       };
+
+      const ProductResponses = () => {
+        return (
+            <div style={{border: '1px solid #ccc', margin: '10px', padding: '10px'}}>
+                {showTitles.map((product, index) => (
+                    <div key={index} style={{ display: 'flex', alignItems: 'center', margin: '5px 0' }}>
+                        <p style={{ margin: 0, padding: '5px', flexGrow: 1 }}>
+                            {product || 'Unnamed Product'}
+                        </p>
+                        {responses[index] == 'Success' ? (
+                            <i className="pi pi-check" style={{fontSize: '2em', color: 'green'}}></i> // Success icon
+                        ) : (
+                            <i className="pi pi-times" style={{fontSize: '2em', color: 'red'}}></i> // Failure icon
+                        )}
+                    </div>
+                ))}
+            </div>
+        );
+    };
     
     const handleDialog = (products: any[]) => {
         setShowDialog(true)
@@ -74,7 +93,13 @@ export const SyndigoSubmitButton = ({ products, URL, buttonText }: SyndigoSubmit
         }
     
         const response = await axios.post(URL + 'update-products', formatProduct);
+
+        setResponses(productsArray?.map((product, index) => {
+            let pResponse = response.data.responseData.ResultsByActionType.ProductImportData[index].Result
+            return pResponse || 'Unnamed Product'
+        }));
         setShowDialog(false);
+        setShowResult(true);
     }
 
 
@@ -99,6 +124,21 @@ export const SyndigoSubmitButton = ({ products, URL, buttonText }: SyndigoSubmit
                     label="Cancel"
                     onClick={() => setShowDialog(false)}
                 />
+            </Dialog>
+
+            <Dialog
+                visible={showResult}
+                style={{ width: '30vw', height: '15vw' }}
+                onHide={() => setShowResult(false)}
+            > 
+                {ProductResponses()}
+                <Button
+                    className="p-button-danger ml-3 my-3 align-items-end"
+                    icon="pi pi-ban"
+                    label="Close"
+                    onClick={() => setShowResult(false)}
+                />
+                
             </Dialog>
         </>
     )};
